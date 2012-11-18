87b4f5cf79eab0f3a2697b6823b5e961459271db
Carl Markham <doddsey65@hotmail.com>
2012-11-18 18:36:46 +0000

    added space calculation

--- index.php
+++ index.php
@@ -1,6 +1,13 @@
 <?php
 error_reporting(E_ALL & ~E_NOTICE);
 
+include '../../header.html';
+include 'config.php';
+include 'db.php';
+
+Db::$local = Db::connect($db_config['local']);
+Db::$master = Db::connect($db_config['master']);
+
 function sanitize($str)
 {
 	$orig = $str;
@@ -56,56 +63,56 @@ function sanitize($str)
 			$after = 3;
 
 			$string_words = str_word_count($str,1);
-		    $word_position_pos = array_keys(str_word_count($str, 2));
+			$word_position_pos = array_keys(str_word_count($str, 2));
 
-		    $count = 0;
-		    $found = array();
+			$count = 0;
+			$found = array();
 
-		    while ($count < $max_count) 
-		    {
-		        if (($word_position = array_search($w,$string_words)) === false)
-		        {
-		            break;
-		        }
+			while ($count < $max_count) 
+			{
+				if (($word_position = array_search($w,$string_words)) === false)
+				{
+					break;
+				}
 
-		        ++$count;
-		        if (($word_position + $after) >= count($string_words))
-		        {
-		            $after = count($string_words) - $word_position - 1;
-		        }
+				++$count;
+				if (($word_position + $after) >= count($string_words))
+				{
+					$after = count($string_words) - $word_position - 1;
+				}
 
-		        $start_pos = $word_position_pos[$word_position - $before];
-		        $end_pos = $word_position_pos[$word_position + $after] + strlen($string_words[$word_position + $after]);
+				$start_pos = $word_position_pos[$word_position - $before];
+				$end_pos = $word_position_pos[$word_position + $after] + strlen($string_words[$word_position + $after]);
 
-		        $string_words = array_slice($string_words,$word_position + 1);
-		        $word_position_pos = array_slice($word_position_pos, $word_position + 1);
+				$string_words = array_slice($string_words,$word_position + 1);
+				$word_position_pos = array_slice($word_position_pos, $word_position + 1);
 
-		        $found[] = substr($str, $start_pos, $end_pos - $start_pos);
-		    }
+				$found[] = substr($str, $start_pos, $end_pos - $start_pos);
+			}
 
-		    if (!empty($found))
-		    {
-			    foreach ($found as $context)
-			    {
-			    	$_found = explode(' ', $context);
+			if (!empty($found))
+			{
+				foreach ($found as $context)
+				{
+					$_found = explode(' ', $context);
 					$key = array_search($w, $_found);
 					unset($_found[$key]);
-			    	
-			    	if (empty($_found))
-			    	{
-			    		break;
-			    	}
+					
+					if (empty($_found))
+					{
+						break;
+					}
 
-			    	$_words = explode(' ', $context);
+					$_words = explode(' ', $context);
 
-			    	foreach ($_words as $k => $_w)
-			    	{
-			    		if (in_array($_w.$_words[$k+1], $words))
-			    		{
-			    			$str = str_replace(array($_w.' '.$_words[$k+1]), array(str_repeat('*', strlen($_w.$_words[$k+1]))), $str);
-			    		}
-			    	}
-			    }
+					foreach ($_words as $k => $_w)
+					{
+						if (in_array($_w.$_words[$k+1], $words))
+						{
+							$str = str_replace(array($_w.' '.$_words[$k+1]), array(str_repeat('*', strlen($_w.$_words[$k+1]))), $str);
+						}
+					}
+				}
 			}
 		}
 	}
@@ -176,5 +183,51 @@ function sanitize($str)
 		'original' => $orig,
 		'sanitized' => $str
 	));
+
+	if ($str == $orig)
+	{
+		Db::$master->prex("INSERT IGNORE INTO words (word) VALUES (?)", array($str));
+	}
+	else
+	{
+		echo '<br /><br /><h1>Epic Fail <small>You failed to bypass the filter</small></h1>';
+	}
 }
+?>
+
+<div style="text-align:center">
+	<form method="get" class="clearfix">
+		<h2>
+			Profanity Filter
+			<small>Enter a swear word below. Use spaces, symbols, anything you want.</small>
+		</h2>
+		<input value="<?=$_GET['str']?>" type="text" style="font-size:20px;padding:20px;border-radius:10px" class="span6" placeholder="Enter swear word" name="str" /><br />
+		<button style="font-size:20px;padding:20px !important;border-radius:10px;width:490px" class="btn btn-primary">Sanitize</button>
+	</form>
+
+	<?
+	if (isset($_GET['str']))
+	{
+		$str = urldecode($_GET['str']);
+		echo sanitize($str);
+		echo '<hr />';
+	}
+?>
+</div>
+<?
+$recent = array();
+$results = Db::$local->fetch_results("SELECT * FROM words LIMIT 50");
+foreach ($results as $r)
+{
+	$recent[] = '<span class="btn">'.$r['word'].'</span>';
+}
+?>
+
+<fieldset>
+	<legend>Recent Words</legend>
+	<?=implode(',', $recent)?>
+</fieldset>
+
+<?
+include '../../footer.html';
 ?>
