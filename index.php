74356c1187594d2a819808ff052ead8846ebfdf9
Carl Markham <doddsey65@hotmail.com>
2012-11-18 18:41:51 +0000

    added space calculation

--- index.php
+++ index.php
@@ -41,82 +41,13 @@ function sanitize($str)
 		'nigga'
 	);
 
-	// find word and calculate the context in which it was used
-	foreach ($words as $k => $w)
-	{
-		if (strpos($str, $w) !== false)
-		{
-			// get 3 words back and 3 words forward
-			$max_count = 1;
-			$before = 3;
-			$after = 3;
-
-			$string_words = str_word_count($str,1);
-			$word_position_pos = array_keys(str_word_count($str, 2));
-
-			$count = 0;
-			$found = array();
-
-			while ($count < $max_count) 
-			{
-				if (($word_position = array_search($w,$string_words)) === false)
-				{
-					break;
-				}
-
-				++$count;
-				if (($word_position + $after) >= count($string_words))
-				{
-					$after = count($string_words) - $word_position - 1;
-				}
-
-				$start_pos = $word_position_pos[$word_position - $before];
-				$end_pos = $word_position_pos[$word_position + $after] + strlen($string_words[$word_position + $after]);
-
-				$string_words = array_slice($string_words,$word_position + 1);
-				$word_position_pos = array_slice($word_position_pos, $word_position + 1);
-
-				$found[] = substr($str, $start_pos, $end_pos - $start_pos);
-			}
-
-			if (!empty($found))
-			{
-				foreach ($found as $context)
-				{
-					$_found = explode(' ', $context);
-					$key = array_search($w, $_found);
-					unset($_found[$key]);
-					
-					if (empty($_found))
-					{
-						break;
-					}
-
-					$_words = explode(' ', $context);
-
-					foreach ($_words as $k => $_w)
-					{
-						if (in_array($_w.$_words[$k+1], $words))
-						{
-							$str = str_replace(array($_w.' '.$_words[$k+1]), array(str_repeat('*', strlen($_w.$_words[$k+1]))), $str);
-						}
-					}
-				}
-			}
-		}
-	}
-
 	$symbols = array('$', '0', '(');
 	$letters = array('s', 'o', 'c');
 
 	echo '--Orignal String--<br />'.$str.'<br /><br />';
 	$str = str_replace($symbols, $letters, $str);
 	
-	// calculate levenshtein distance and replace words in string
-	// which sound like words in array
 	$check = explode(' ', $str);
-
-	echo '--Calculating levenshtein distance--<br />';
 
 	foreach($check as $k => $c)
 	{
@@ -129,10 +60,6 @@ function sanitize($str)
 
 		foreach ($words2 as $w)
 		{
-			$lev = levenshtein($c, $w);
-						
-			$percentage = (int) round((1 - $lev / max(strlen($c), strlen($w))) * 100, 2);
-			
 			if (metaphone(trim($c)) === metaphone(trim($w)) || $percentage >= 75)
 			{
 				$str = str_replace($c, str_repeat('*', strlen($c)) ,$str);
@@ -143,10 +70,7 @@ function sanitize($str)
 	// simple preg replace of words in array
 	foreach ($words as $w)
 	{
-		if (preg_match_all('/'.$w.'/i', $str, $matches))
-		{
-			$str = preg_replace('/'.$w.'/i', str_repeat('*', strlen($w)), $str);
-		}
+		$str = str_replace($w, str_repeat('*', strlen($w)), $str);
 	}
 	
 	// trim whitespace to replace swears seperated by spaces
@@ -159,10 +83,7 @@ function sanitize($str)
 		$_w = str_split($w);
 		$_w = implode('-', $_w);
 
-		if (preg_match_all('/'.$_w.'/', $str, $match))
-		{
-			$str = preg_replace('/'.$_w.'/', str_repeat('*', strlen($w)), $str);
-		}
+		$str = preg_replace('/'.$_w.'/', str_repeat('*', strlen($w)), $str);
 	}
 
 	$str = str_replace('-', ' ', $str);
